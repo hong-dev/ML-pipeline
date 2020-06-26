68938fce5e2950c3597beda87afaaace6289c27d
soo <soo@solidware.io>
2020-06-26 17:44:27 +0900

    wip: make preprocessor(columntransformer)

--- ml_data.py
+++ ml_data.py
@@ -8,49 +8,23 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_score
 from sklearn.impute import SimpleImputer as SklearnSimpleImputer
 from sklearn.model_selection import train_test_split
+from sklearn.base import TransformerMixin
 
 from sklearn.preprocessing import OneHotEncoder as SklearnOneHotEncoder
 from sklearn.compose import ColumnTransformer as SklearnColumnTransformer
 
-class ColumnTransformer(SklearnColumnTransformer):
-    # def transform(self, X) -> pd.DataFrame:
-    #     return pd.DataFrame(super().transform(X), columns=X.columns, index=X.index)
-
-    # def fit_transform(self, X):
-    #     # X = self.named_transformers_
-    #     return pd.DataFrame(super().fit_transform(X), columns=X.columns, index=X.index)
-
-    def merge(self, X, y=None):
-        return pd.DataFrame(super().fit_transform(X), columns=X.columns, index=X.index)
-
-    def transform(self, X):
-        sparse_matrix = super().fit_transform(X)
-        a = self.transformers[0][1]
-        print(a)
-        print(pd.DataFrame(a).columns)
-        return pd.DataFrame(sparse_matrix.toarray(), columns=a.columns, index=X.index)
-
-    def fit_transform(self, X, y=None):
-        # self.fit(X)
-        return self.transform(X)
-
 class RobustScaler(SklearnRobustScaler):
-    def fit_transform(self, X, y=None):
-        return pd.DataFrame(super().fit_transform(X), columns=X.columns, index=X.index)
+    def transform(self, X):
+        return pd.DataFrame(super().transform(X), columns=X.columns, index=X.index)
 
 class SimpleImputer(SklearnSimpleImputer):
-    def fit_transform(self, X, y=None):
-        return pd.DataFrame(super().fit_transform(X), columns=X.columns, index=X.index)
+    def transform(self, X):
+        return pd.DataFrame(super().transform(X), columns=X.columns, index=X.index)
 
 class OneHotEncoder(SklearnOneHotEncoder):
     def transform(self, X):
-        sparse_matrix = super(OneHotEncoder, self).transform(X)
         new_columns = self.get_new_columns(X=X)
-        return pd.DataFrame(sparse_matrix.toarray(), columns=new_columns, index=X.index)
-
-    def fit_transform(self, X, y=None):
-        self.fit(X)
-        return self.transform(X)
+        return pd.DataFrame(super().transform(X).toarray(), columns=new_columns, index=X.index)
 
     def get_new_columns(self, X):
         new_columns = []
@@ -61,42 +35,81 @@ class OneHotEncoder(SklearnOneHotEncoder):
                 j += 1
         return new_columns
 
+class TestSplit:
+    def split_test(self, dataset, target):
+        self.features_without_target = dataset.drop(target, axis=1)
+        self.target_feature = dataset[target]
+
+        X_train, X_test, y_train, y_test = train_test_split(self.features_without_target, self.target_feature)
+        return X_train, X_test, y_train, y_test
+
+class Preprocessor(ColumnTransformer):
+    def __init__(self, dataset):
+        self.numeric_features = dataset.select_dtypes(np.number)
+        self.categorical_features = dataset.select_dtypes(exclude=np.number)
+        super().__init__(transformers= [
+            ('nums', self.transform_numeric_features(), self.numeric_features.columns),
+            ('cats', self.transform_categorical_features(), self.categorical_features.columns)
+        ]
+
+    @staticmethod
+    def transform_numeric_features():
+        return Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='median')),
+            ('scaler', RobustScaler())
+            ])
+
+    @staticmethod
+    def transform_categorical_features():
+        return Pipeline(steps=[
+            ('imputer', SimpleImputer(strategy='most_frequent')),
+            ('onehot', OneHotEncoder())
+            ])
+
+    # def transform_columns(self):
+    #     return ColumnTransformer(transformers = [
+    #         ('nums', self.transform_numeric_features(), self.numeric_features.columns),
+    #         ('cats', self.transform_categorical_features(), self.categorical_features.columns)
+    #         ])
+
+    def get_column_names(self):
+        nums_names = self.transformers['nums'].fit_transform(self.numeric_features).columns
+        cats_names = self.transformers['cats'].fit_transform(self.categorical_features).columns #implement.named_transformers_['cats'].fit_transform(self.categorical_features).columns
+        feat_names = np.concatenate([nums_names, cats_names])
+        return feat_names
+
+    # def fit(self, X):
+    #     # self.dataset = X
+    #     # self.numeric_features = X.select_dtypes(np.number)
+    #     # self.categorical_features = X.select_dtypes(exclude=np.number)
+    #     # self.implement = self.transform_columns()
+    #     # self.transformed_data = self.implement.fit_transform(X)
+    #     return self
+
+    def transform(self, X):
+        column_names = self.get_column_names()
+        print("HELLO!!!IMHERE")
+        return pd.DataFrame(super().transform(X).toarray(), columns=column_names, index=X.index)
+
+        # column_names = self.get_column_names()
+        # return pd.DataFrame(self.transformed_data, columns=column_names, index=X.index)
+
+
+
+
 
 train_data = pd.read_csv('./data/marketing_train.csv')
-test_data = pd.read_csv('./data/marketing_test.csv')
 
-# target = 'insurance_subscribe'
+a = TestSplit().split_test(train_data, "insurance_subscribe")
+# print(a[0])
 
-# X = train_data.drop(target, axis=1)
-# y = train_data[target]
+b = Preprocessor(a[0]).fit_transform(a[0])
+print(b)
 
-# X_train, X_test, y_train, y_test = train_test_split(X, y)
+########################################
 
-# onehot_marketing = OneHotEncoder().fit_transform(marketing.select_dtypes(include=['object']))
-
-# onehot_data = pd.DataFrame(onehot_marketing.toarray())
-
-
-# process = make_column_transformer(
-#     (OneHotEncoder(categories='auto'), [0]),
-#     remainder='passthrough'
-# )
-
-
-numeric_features = train_data.select_dtypes(np.number).columns
-categorical_features = train_data.select_dtypes(['object', 'bool', 'category']).columns
-
-# numeric_features = train_data.select_dtypes(exclude=['object']).columns
-# categorical_features = train_data.select_dtypes(include=['object']).columns
-
-# numeric_features = make_column_selector(marketing, dtype_exclude='object')
-# categorical_features = make_column_selector(marketing, dtype_include='object')
-##TypeError: 'DataFrame' objects are mutable, thus they cannot be hashed
-
-# numeric_features = list(train_data.columns[train_data.dtypes == 'int64'])
-# categorical_features = list(train_data.columns[train_data.dtypes == 'object'])
-
-
+numeric_features = X_train.select_dtypes(np.number)
+categorical_features = X_train.select_dtypes(exclude=np.number) #object, bool, category
 
 numeric_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='median')),
@@ -105,71 +118,54 @@ numeric_transformer = Pipeline(steps=[
 
 categorical_transformer = Pipeline(steps=[
     ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('onehot', OneHotEncoder()), #handle_unknown='ignore'
+    ('onehot', OneHotEncoder()) #handle_unknown='ignore'
 ])
-
 
 preprocessor = ColumnTransformer(
     transformers = [
-        ('nums', numeric_transformer, numeric_features),
-        ('cats', categorical_transformer, categorical_features)
+        ('nums', numeric_transformer, numeric_features.columns),
+        ('cats', categorical_transformer, categorical_features.columns)
     ] #n_jobs=-1
 )
 
-## ValueError: No valid specification of the columns. Only a scalar, list or slice of all integers or all strings, or boolean mask is allowed
-## df.select_dtypes() doesn't output column indices. It outputs a subset of the DataFrame with the matched columns.
+########################################
 
-# print(marketing.select_dtypes(include=['object']))
-# print(make_column_selector(marketing, dtype_include='object'))
+preprocessor_fit = preprocessor.fit_transform(X_train) ## -1~1  <- -44 999 1 -44 -1 scaler, ohe A a,b,c A_a, A_b, A_c
+#preprocessor.tranform(test_data) ## a, b A_a, A_b   30 ~ 50 30 -1 50 1 -0.03 -
 
-
-# print(OneHotEncoder().fit_transform(train_data.select_dtypes(['object'])))
-# print(SimpleImputer(strategy='most_frequent').fit_transform(train_data.select_dtypes(['object'])))
-# print(categorical_transformer.fit_transform(train_data.select_dtypes(['object'])))
-
-# print(SimpleImputer(strategy='median').fit_transform(train_data.select_dtypes(np.number)))
-# print(RobustScaler().fit_transform(train_data.select_dtypes(np.number)))
-# print(numeric_transformer.fit_transform(train_data.select_dtypes(np.number)))
+nums_names = preprocessor.named_transformers_['nums'].fit_transform(numeric_features).columns
+cats_names = preprocessor.named_transformers_['cats'].fit_transform(categorical_features).columns
+feat_names = np.concatenate([nums_names, cats_names])
 
 
-preprocessor_fit = preprocessor.fit_transform(train_data)
-p = preprocessor.fit_transform(train_data)
-# print(preprocessor.named_transformers_['cats'].fit_transform(train_data.select_dtypes(['object'])))
-
-print(pd.DataFrame(p))
-
-#### get column names
-# cats_names = preprocessor.named_transformers_['cats']['onehot'].get_feature_names(categorical_features) #순서가 어찌되는지 찾아보기!
-# cats_names = preprocessor.fit_transform(train_data).get_feature_names() #순서가 어찌되는지 찾아보기!
-# print(cats_names)
-
-# # pre2 = preprocessor.named_transformers_['nums']['scaler'].get_feature_names()
-# # AttributeError: 'StandardScaler' object has no attribute 'get_feature_names'
-
-# labels = np.concatenate([numeric_features, cats_names]) #순서가 어찌되는지 찾아보기!
-# print(labels)
-
-# print(pd.DataFrame(preprocessor_fit, columns = labels))
-
-
+preprocessed_train = pd.DataFrame(preprocessor_fit, columns=feat_names)
 
 pipe = Pipeline([
     ('preprocessor', preprocessor),
-    ('claasifier', LogisticRegression(solver='lbfgs')) ##classifier
+    ('classifier', LogisticRegression())
 ])
 
-# pipe.fit(X, y)
+
+model = LogisticRegression()
+
+model.fit(preprocessed_train, y_train)
+model.predict(preprocessed_train)
+# print(preprocessed_train)
+
+preprocessed_test = preprocessor.transform(X_test)
+# print(preprocessed_test)
+model.predict(preprocessed_test)
+
+# print(model.score(preprocessed_train, y_train))
+# print(model.score(preprocessed_test, y_test))
+
+# print(pipe.fit_transform(X_train))
+
+##2. STOP이 나오는 이유???!
+
+test_data = pd.read_csv('./data/marketing_test.csv')
 
 
-# print(numeric_transformer.fit_transform(marketing.select_dtypes(exclude=['object'])))
-# print(categorical_transformer.fit_transform(marketing.select_dtypes(include=['object'])).toarray())
 
-
-#cross_val_score(pipe, x, y, cv=5, scoring='accuracy').mean()
-#grid = GridSearchCV(model_pipeline, param_grid=param, cv=5).fit(X_train, y_train)
-#grid.score(X_test, y_test)
-
-
-# print(marketing.dtypes)
-# print(marketing.isna().sum())
-# print(marketing_test.isnull().sum())
+# pred.csv : ID, Predicted value
+# report.csv : Precision, Recall, Accuracy, F1
