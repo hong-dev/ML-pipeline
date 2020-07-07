6665a1d9b7760069a698db06c38004a25fac36c2
soo <soo@solidware.io>
2020-07-07 17:46:19 +0900

    wip: use pd.concat next step: try to make get_scores func and get_score_dataframe func

--- ml_data.py
+++ ml_data.py
@@ -84,9 +84,9 @@ def main():
         named_tuple(index[0], index[1]) for index in scaler_model_combination
     ]
 
-    # create dataframes for prediction and report
+    # create prediction dataframe and score dataframe list
     prediction_df = pd.DataFrame()
-    report_df = pd.DataFrame()
+    score_df_list = []
 
     # transform, predict, score for each combination
     for index in index_combination:
@@ -106,13 +106,18 @@ def main():
             f"{index.scaler.__name__}-{index.model.__name__}"
         ] = test_prediction
 
-        # add scores to report dataframe
-        scores = {
-            "train": train_score,
-            "validation": validation_score,
-            "test": test_score,
-        }
-        report_df = add_report(report_df, index.scaler, index.model, scores)
+        # add score dataframes to the list
+        # scores = {
+        #     "train": train_score,
+        #     "validation": validation_score,
+        #     "test": test_score,
+        # }
+        # score_df = get_score_dataframe(index.scaler, index.model, scores)
+        # score_df_list.append(score_df)
+        score_df_list.extend([train_score, validation_score, test_score])
+
+    # concatenate score dataframes
+    report_df = pd.concat(score_df_list)
 
     # save prediction and report to csv files
     arguments = get_arguments()
@@ -163,7 +168,12 @@ def process_train_data(preprocessor, model):
     train_score = get_scores(y_train, X_train_prediction)
     validation_score = get_scores(y_validation, X_validation_prediction)
 
-    return train_score, validation_score
+    #######
+    a = get_score_dataframe(preprocessor.scaler, model, "train", train_score)
+    b = get_score_dataframe(preprocessor.scaler, model, "validation", validation_score)
+
+    # return train_score, validation_score
+    return a, b
 
 
 def process_test_data(preprocessor, model):
@@ -191,7 +201,11 @@ def process_test_data(preprocessor, model):
     # score
     test_score = get_scores(test_data[target], test_prediction)
 
-    return test_prediction, test_score
+    ##############
+    a = get_score_dataframe(preprocessor.scaler, model, "test", test_score)
+
+    # return test_prediction, test_score
+    return test_prediction, a
 
 
 def get_scores(actual_y, predicted_y):
@@ -209,26 +223,41 @@ def get_scores(actual_y, predicted_y):
     return scores
 
 
-def add_report(report, scaler, model, scores):
-    for data_name, score in scores.items():
+def get_score_dataframe(scaler, model, data_name, scores):
 
-        # get multi-index for report dataframe
-        report_index = pd.MultiIndex.from_tuples(
-            [(scaler.__name__, model.__name__, data_name)],
-            names=["scaler", "model", "data"],
-        )
+    # get multi-index for report dataframe
+    report_index = pd.MultiIndex.from_tuples(
+        [(scaler.__name__, model, data_name)],
+        names=["scaler", "model", "data"],
+    )
 
-        # create dataframe for scores
-        score_data = pd.DataFrame(
-            [score],
-            columns=["Precision", "Recall", "Accuracy", "F1"],
-            index=report_index,
-        )
+    # create dataframe for scores
+    score_data = pd.DataFrame(
+        [scores],
+        columns=["Precision", "Recall", "Accuracy", "F1"],
+        index=report_index,
+    )
 
-        # append score data to original report
-        report = report.append(score_data)
+    return score_data
+ 
+# def get_score_dataframe(scaler, model, scores):
+#     for data_name, score in scores.items():
 
-    return report
+#         # get multi-index for report dataframe
+#         report_index = pd.MultiIndex.from_tuples(
+#             [(scaler.__name__, model.__name__, data_name)],
+#             names=["scaler", "model", "data"],
+#         )
+
+#         # create dataframe for scores
+#         score_data = pd.DataFrame(
+#             [score],
+#             columns=["Precision", "Recall", "Accuracy", "F1"],
+#             index=report_index,
+#         )
+
+#     return score_data
+# 
 
 
 if __name__ == "__main__":
